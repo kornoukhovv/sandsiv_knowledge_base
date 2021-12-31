b634e746703f581c608db4de0f3e8860e4d444d4
sandsiv <daniil.morzhevskiy@sandsiv.com>
2021-12-31 14:49:22 +0200

    backbone setup some minor label name changes added 1st article

--- src/components/HomepageFeatures.js
+++ src/components/HomepageFeatures.js
@@ -4,7 +4,7 @@ import styles from './HomepageFeatures.module.css';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
+    title: 'Easy',
     Svg: require('../../static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
